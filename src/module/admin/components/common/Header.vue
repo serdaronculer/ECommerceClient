3722badbdad1ddbbc1a294c032ae0525d75e104a
Serdar <serdar.onculer.work@hotmail.com>
2022-10-18 01:14:01 +0300

    demo components added

---
+++ src/module/admin/components/common/Header.vue
@@ -0,0 +1,4 @@
+<template>
+    <p>Header</p>
+</template>
+
---
+++ src/module/admin/components/common/Sidebar.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-card
+    class="mx-auto"
+  >
+    <v-list density="compact">
+      <v-list-subheader>Menü</v-list-subheader>
+      <v-list-item
+        v-for="(category, i) in categories"
+        :key="i"
+        :value="category"
+        active-color="#3f51b5"
+        :exact="true"
+        :to="{name:category.link}       
+        "
+      >
+        <v-list-item-title v-text="category.name"></v-list-item-title>
+      </v-list-item>
+    </v-list>
+  </v-card>
+
+</template>
+
+<script lang="ts" setup>
+
+import { ref } from '@vue/reactivity'
+
+const categories = ref<{name: string, link: string}[]>([ {
+          name: 'Dashboard',
+          link: 'dashboard',
+        },
+        {
+          name: 'Customers',
+          link: 'customers'
+        },
+        {
+          name: 'Products',
+          link: 'productsAdmin'
+        },
+        {
+          name: 'Orders',
+          link: 'orders'
+        },
+        {
+          name: 'Go to the site',
+          link: 'home'
+        }
+        ]);
+</script>
+
+<style lang="scss" scoped>
+
+.oba{
+  color: green;
+}
+</style>
