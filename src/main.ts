04de76a892350990ba45f044527fb1fe8affb5fb
Serdar <serdar.onculer.work@hotmail.com>
2022-10-18 01:07:00 +0300

    commit

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,16 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import vuetify from './plugins/vuetify'
+import { loadFonts } from './plugins/webfontloader'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min.js'
 
-createApp(App).use(store).use(router).mount('#app')
+
+loadFonts()
+
+createApp(App)
+  .use(router)
+  .use(store)
+  .use(vuetify)
+  .mount('#app')
--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -2,5 +2,10 @@
 declare module '*.vue' {
   import type { DefineComponent } from 'vue'
   const component: DefineComponent<{}, {}, any>
+   declare global {
+    interface Window { alertify: any;}
+  }
   export default component
 }
+
+
