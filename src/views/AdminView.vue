8e184bb2aff4aa194b2044f024e4ab4550828bce
Serdar <serdar.onculer.work@hotmail.com>
2022-10-18 01:07:26 +0300

    views demo

---
+++ src/views/AdminView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="bg-surface-variant">
+    <v-row no-gutters>
+      <v-col cols="12">
+        <v-sheet class="pa-2"> HEADER </v-sheet>
+      </v-col>
+      <v-col cols="3">
+        <v-sheet class="pa-2">
+          <Sidebar></Sidebar>
+        </v-sheet>
+      </v-col>
+      <v-col cols="9">
+        <v-sheet class="pa-2">
+          <router-view></router-view>
+        </v-sheet>
+      </v-col>
+      <v-col cols="12">
+        <v-sheet class="pa-2"> FOOTER</v-sheet>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import Sidebar from "@/module/admin/components/common/Sidebar.vue";
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,51 @@
+<template>
+  <nav class="navbar navbar-expand-lg bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Mini E-Commerce</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <router-link exact-active-class="router-link-active" class="nav-link" :to="{ name: 'home' }">Home</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link exact-active-class="router-link-active" class="nav-link" :to="{ name: 'productsUi' }">Products</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link exact-active-class="router-link-active" class="nav-link" :to="{ name: 'basket' }">Basket</router-link>
+          </li>
+        </ul>
+        <form class="d-flex" role="search">
+          <input
+            class="form-control me-2"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+          />
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+      </div>
+    </div>
+  </nav>
+</template>
+
+
+<script lang="ts" setup>
+
+</script>
+
+<style scoped>
+.router-link-active {
+    color: #198754 !important;
+}
+</style>
