8507bf525cbbfc224d727f8ab7c65551c6fe3f3d
Serdar <serdar.onculer.work@hotmail.com>
2022-10-18 01:18:11 +0300

    basic routes added

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,20 +1,33 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import AdminView from '../views/AdminView.vue'
+import Dashboard from '@/module/admin/components/dashboard/Dashboard.vue';
+import Home from '@/views/Home.vue'
+
+
 
 const routes: Array<RouteRecordRaw> = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: "/admin",
+    name: "admin",
+    component: AdminView,
+    children: [
+      { path: '', name:"dashboard" ,component: Dashboard },
+      { path: "products", name:"productsAdmin", component: () => import('@/module/admin/components/products/Product.vue') },
+      { path: "customers", name:"customers", component: () => import('@/module/admin/components/customers/Customer.vue') },
+      { path: "orders", name:"orders", component: () => import('@/module/admin/components/orders/Order.vue') },
+    ],
   },
+
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: "/",
+    name: "home",
+    component: Home,
+    children: [
+      {path:"basket", name:"basket", component: () => import('@/module/ui/components/baskets/Basket.vue')},
+      {path:"products", name:"productsUi", component: () => import('@/module/ui/components/products/Product.vue')},
+    ]
   }
+
 ]
 
 const router = createRouter({
